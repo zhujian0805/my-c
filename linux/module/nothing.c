7062bea32e5bab4e6f8c64c15f6f24d7ee08e14e
Nemo Zhu <zhujian0805@gmail.com>
2015-10-25 17:13:15 +0800

    ?? ../

---
+++ linux/module/nothing.c
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  nothing.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2015年10月25日 17时12分02秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  James Zhu (N/A), zhujian0805@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <linux/module.h>
