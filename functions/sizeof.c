1dda16f24b6858d1d84b9493d44e0517dd42d5a2
Nemo Zhu <zhujian0805@gmail.com>
2015-10-25 11:40:29 +0800

    ?? ./

---
+++ functions/sizeof.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  sizeof.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2015年10月25日 11时33分39秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  James Zhu (N/A), zhujian0805@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+       printf("Storage size for int : %ld \n", sizeof(int));
+       return(0);
+}
