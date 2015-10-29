9653e0ce2e3de64fcf11e744213612ba8767f8b6
James Zhu <zhujian0805@gmail.com>
2015-10-29 16:57:12 +0800

    M pointer01.c ?? a.out

--- pointers/pointer01.c
+++ pointers/pointer01.c
@@ -16,11 +16,13 @@ int main ()
 
    /* take the value using pptr */
    printf("Value available at var = %d\n", var);
-   printf("Address of var = %p\n", &var);
-   printf("Value available at *pptr = %d\n", *ptr);
-   printf("Value available at *pptr = %p\n", &ptr);
-   printf("Value available at **pptr = %d\n", **pptr);
-   printf("Value available at **pptr = %p\n", &pptr);
+   printf("Value available at var = %p\n", &var);
+
+   printf("Value available at var = %p\n", ptr);
+   printf("Value available at var = %p\n", &ptr);
+
+   printf("Value available at var = %p\n", pptr);
+   printf("Value available at var = %p\n", &pptr);
 
    return 0;
 }
